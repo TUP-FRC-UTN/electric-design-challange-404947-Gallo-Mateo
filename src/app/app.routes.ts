fb83c72b743d31045feaa0d53b3d61028bb6e178
404947_Gallo_Mateo <404947@tecnicatura.frc.utn.edu.ar>
2024-11-15 00:52:25 -0300

    repasando como dios manda

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { BudgetFormComponent } from './budget-form/budget-form.component';
+import { BudgetListComponent } from './budget-list/budget-list.component';
+import { BudgetViewComponent } from './budget-view/budget-view.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'create-budget', component: BudgetFormComponent
+    },
+    {
+        path: 'budgets', component: BudgetListComponent
+    },
+    {
+        path: 'budgets/:id', component: BudgetViewComponent
+    },
+    {
+        path: '', component: BudgetListComponent
+    },
+    // {
+    //     //ruta "comodin" (si se pone una url no especificada, se redirige al component especificado)
+    //     path: '**', component: OrdersComponent
+    // },
+];
